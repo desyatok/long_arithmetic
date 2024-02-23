589c284caecd7e47ede55ee63d99f831788725b3
Pavel Desyatnikov <desyatnikovpvl@gmail.com>
2024-02-23 12:58:21 +0300

    Create interface
    Add bignum struct and function prototypes for standard integer numbers operations (+,-,*,/,%), initializing, deleting and printing bignum and converting it to a string.

--- bignum_lib.h
+++ bignum_lib.h
@@ -1,6 +1,34 @@
 #ifndef BIGNUM_LIB_H
 #define BIGNUM_LIB_H
 
-void hello(void);
+typedef struct bignum
+{
+    char *digits;
+    int size;
+    int sign; // хранит "1", если число неотрицательное, иначе "-1"
+} bignum_t;
+
+typedef bignum_t *bignum_ptr;
+
+void print_number(bignum_ptr num);
+
+bignum_ptr num_init(char *str); /* принимает строку с big-endian числом,
+                                    * возвращает структуру с little-endian
+                                    * используется для конвертации строки в структуру */
+
+bignum_ptr add(bignum_ptr num1, bignum_ptr num2);
+
+bignum_ptr subtract(bignum_ptr num1, bignum_ptr num2);
+
+bignum_ptr multiply(bignum_ptr num1, bignum_ptr num2);
+
+bignum_ptr divide(bignum_ptr num1, bignum_ptr num2);
+
+bignum_ptr division_remainder(bignum_ptr num1, bignum_ptr num2);
+
+char *number_to_string(bignum_ptr num);
+
+void free_num(bignum_ptr num);
+
 
 #endif //BIGNUM_LIB_H
