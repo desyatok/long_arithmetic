2cceb26bf54baee5d9631af4f05d707c21a76a10
Pavel Desyatnikov <desyatnikovpvl@gmail.com>
2024-02-23 15:13:48 +0300

    Implement addition and subtraction
    Auxiliary functions add_ui and subtract_ui (operations, which perform like the nums are unsigned) also were implemented (not a part of the library).

--- bignum_lib.c
+++ bignum_lib.c
@@ -137,3 +137,157 @@ char *number_to_string(const bignum_t *num)
     if (num->sign == -1) str--;
     return str;
 }
+
+int compare(const bignum_t *num1, const bignum_t *num2)
+{
+    if (num1->size > num2->size)
+    {
+        return 1;
+    }
+    else if (num1->size < num2->size)
+    {
+        return -1;
+    }
+
+    for (int i = num1->size - 1; i >= 0; i--)
+    {
+        if (num1->digits[i] > num2->digits[i])
+        {
+            return 1;
+        }
+        else if (num1->digits[i] < num2->digits[i])
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+bignum_t *add_ui(const bignum_t *num1, const bignum_t *num2)
+{
+    if (num1 == NULL || num2 == NULL) return NULL;
+
+    bignum_t * result = malloc(sizeof(bignum_t));
+    result->size = max(num1->size, num2->size);
+    result->sign = num1->sign;
+    result->digits = malloc(result->size);
+
+    int carry = 0;
+
+    for (size_t i = 0; i < result->size; i++)
+    {
+        result->digits[i] = carry + (i < num1->size ? num1->digits[i] - '0' : 0)
+                            + (i < num2->size ? num2->digits[i] - '0' : 0) + '0';
+        carry = result->digits[i] - '0' >= BASE;
+        if (carry)
+        {
+            result->digits[i] -= BASE;
+        }
+    }
+
+    if (carry)
+    {
+        result->size++;
+        result->digits = realloc(result->digits, result->size);
+        result->digits[result->size - 1] = carry + '0';
+    }
+    return result;
+}
+
+bignum_t *subtract_ui(const bignum_t *num1, const bignum_t *num2)
+{
+    bignum_t * result = malloc(sizeof(bignum_t));
+    result->size =  num1->size;
+    result->digits = malloc(result->size * sizeof(char));
+
+    int borrow = 0;
+
+    for (size_t i = 0; i < result->size; i++)
+    {
+
+        result->digits[i] = (i < num1->size ? num1->digits[i] - '0' : 0)
+                            - (i < num2->size ? num2->digits[i] - '0' : 0) + '0' - borrow;
+        borrow = result->digits[i] - '0' < 0;
+        if (borrow)
+        {
+            result->digits[i] += BASE;
+        }
+    }
+
+    if (result->digits[result->size - 1] == '0' && result->size != 1)
+    {
+        if (!delete_leading_zeros(result))
+        {
+            free_num(result);
+            return NULL;
+        }
+    }
+
+    return result;
+}
+
+bignum_t *add(const bignum_t *num1, const bignum_t *num2)
+{
+    if (num1 == NULL || num2 == NULL) return NULL;
+    if (num1->sign * num2->sign == 1)
+    {
+        return add_ui(num1, num2);
+    }
+    else
+    {
+        bignum_t *result = malloc(sizeof(bignum_t));
+        int cmp = compare(num1,num2);
+
+        if (cmp > 0)
+        {
+            result = subtract_ui(num1, num2);
+            result->sign = num1->sign * cmp;
+        }
+        else if (cmp < 0)
+        {
+            result = subtract_ui(num2, num1);
+            result->sign = num1->sign * cmp;
+        }
+        else
+        {
+            result->size = 1;
+            result->sign = 1;
+            result->digits = malloc(sizeof(char));
+            result->digits[0] = '0';
+        }
+        return result;
+    }
+}
+
+bignum_t *subtract(const bignum_t *num1, const bignum_t *num2)
+{
+    if (num1 == NULL || num2 == NULL) return NULL;
+    if (num1->sign * num2->sign == 1)
+    {
+        bignum_t *result = malloc(sizeof(bignum_t));
+        int cmp = compare(num1,num2);
+
+        if (cmp > 0)
+        {
+            result = subtract_ui(num1, num2);
+            result->sign = num1->sign * cmp;
+        }
+        else if (cmp < 0)
+        {
+            result = subtract_ui(num2, num1);
+            result->sign = num1->sign * cmp;
+        }
+        else
+        {
+            result->size = 1;
+            result->sign = 1;
+            result->digits = malloc(sizeof(char));
+            result->digits[0] = '0';
+        }
+        return result;
+    }
+    else
+    {
+        return add_ui(num1, num2);
+    }
+}
